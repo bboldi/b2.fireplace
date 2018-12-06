b31733321ab0f7432a7fb3ca37d54b13c1c1b3bb
Bednarik Boldizsar <bbednarik@gmail.com>
2018-12-06 16:38:22 +0100

    Added brightness change and optimized code

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,41 +6,48 @@
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
 
-#define DELAY_PER_CYCLE 20
-#define ADD_PARTICLE_CYCLE 2
-#define POSTPROCESS_CYCLE 3
+#define DELAY_PER_CYCLE 30
+#define ADD_PARTICLE_CYCLE 1
+#define POSTPROCESS_CYCLE 2
 
 #define WIDTH 5
 #define HEIGHT 16
-#define NUM_LEDS WIDTH*HEIGHT
+#define NUM_LEDS WIDTH *HEIGHT
 #define DO_MASK true
+
+#define MAX_BRIGHTNESS 240
+#define MIN_BRIGHTNESS 30
 
 #define COLOR_MAX_VALUE 255
 
 float masking[16][5] = {
-  {0.1, 0.2,  1.0,  0.2,  0.1},
-  {0.2, 0.3,  1.0,  0.3,  0.2},
-  {0.3, 0.4,  1.0,  0.4,  0.3},
-  {0.4, 0.5,  1.0,  0.5,  0.4},
-  {0.5, 0.7,  1.0,  0.7,  0.5},
-  {0.6, 0.9,  1.0,  0.9,  0.6},
-  {0.7, 1.0,  1.0,  1.0,  0.7},
-  {0.8, 1.0,  1.0,  1.0,  0.8},
-  {0.9, 1.0,  1.0,  1.0,  0.9},
-  {1.0, 1.0,  1.0,  1.0,  1.0},
-  {1.0, 1.0,  1.0,  1.0,  1.0},
-  {1.0, 1.0,  1.0,  1.0,  1.0},
-  {1.0, 1.0,  1.0,  1.0,  1.0},
-  {1.0, 1.0,  1.0,  1.0,  1.0},
-  {0.4, 1.0,  1.0,  1.0,  0.4},
-  {0.2, 0.5,  1.0,  0.5,  0.2}
-};
+    {0.1, 0.2, 1.0, 0.2, 0.1},
+    {0.1, 0.3, 1.0, 0.3, 0.1},
+    {0.2, 0.4, 1.0, 0.4, 0.2},
+    {0.2, 0.5, 1.0, 0.5, 0.2},
+    {0.3, 0.7, 1.0, 0.7, 0.3},
+    {0.3, 0.9, 1.0, 0.9, 0.3},
+    {0.7, 1.0, 1.0, 1.0, 0.7},
+    {0.8, 1.0, 1.0, 1.0, 0.8},
+    {0.9, 1.0, 1.0, 1.0, 0.9},
+    {1.0, 1.0, 1.0, 1.0, 1.0},
+    {1.0, 1.0, 1.0, 1.0, 1.0},
+    {1.0, 1.0, 1.0, 1.0, 1.0},
+    {1.0, 1.0, 1.0, 1.0, 1.0},
+    {1.0, 1.0, 1.0, 1.0, 1.0},
+    {0.4, 1.0, 1.0, 1.0, 0.4},
+    {0.2, 0.5, 1.0, 0.5, 0.2}};
 
 float convolutionMatrix[3][3] = {
-  {0,0,0},
-  {0,0.7,0},
-  {0.2,1,0.2}
-    };
+    {0, 0, 0},
+    {0, 0.7, 0},
+    {0.2, 1, 0.2}};
+
+int palette[3][3] = {
+    {200, 44, 12},
+    {100, 55, 0},
+    {245, 245, 12}
+};
 
 float convolutionDivider = 2.2;
 
@@ -93,34 +100,18 @@ void addNewParticle()
 {
   particleAddedCnt++;
 
-  if(particleAddedCnt>=ADD_PARTICLE_CYCLE)
+  if (particleAddedCnt >= ADD_PARTICLE_CYCLE)
   {
-    particleAddedCnt=0;
+    particleAddedCnt = 0;
     int rx = rand() % WIDTH;
     int ry = ceil(5 * HEIGHT / 6 + rand() % HEIGHT / 6);
 
     int _ct = rand() % 2;
-    int _rc = 1+rand() % 6;
+    int _rc = 1 + rand() % 6;
 
-    switch(_ct){
-
-      case 0:
-        display[rx][ry][0] = (int)((float)200 / (float)_rc);
-        display[rx][ry][1] = (int)((float)44 / (float)_rc);
-        display[rx][ry][2] = (int)((float)12 / (float)_rc);
-      break;
-
-      case 1:
-        display[rx][ry][0] = (int)((float)100 / (float)_rc);
-        display[rx][ry][1] = (int)((float)55 / (float)_rc);
-        display[rx][ry][2] = 0;
-      break;
-
-      case 2:
-        display[rx][ry][0] = (int)((float)245 / (float)_rc);
-        display[rx][ry][1] = (int)((float)245 / (float)_rc);
-        display[rx][ry][2] = (int)((float)12 / (float)_rc);
-      break;
+    for (int p = 0; p < 3; p++)
+    {
+      display[rx][ry][p] = (int)((float)palette[_ct][p] / (float)_rc);
     }
   }
 }
@@ -146,14 +137,13 @@ void setLedByCoord(int x, int y, int _color[3])
   int idy = ((x % 2 == 0) ? HEIGHT - y - 1 : y);
   int index = (x * HEIGHT) + idy;
 
-
   int _cr = _color[0];
   int _cg = _color[1];
   int _cb = _color[2];
 
   // apply mask
 
-  if(DO_MASK)
+  if (DO_MASK)
   {
     _cr = (int)((float)_cr * (float)masking[y][x]);
     _cg = (int)((float)_cg * (float)masking[y][x]);
@@ -166,80 +156,96 @@ void setLedByCoord(int x, int y, int _color[3])
 }
 
 int _postProcess[4][5][3] = {
-  {{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}},
-  {{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}},
-  {{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}},
-  {{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}}
-};
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}};
 
 void postProcessCalculate()
 {
 
   float mask[4][5] = {
-    {0.1,0.1,0.5,0.1,0.1},
-    {0.2,0.5,1,0.5,0.2},
-    {0.5,1,1,1,0.5},
-    {0.5,1,1,1,0.5}
-  };
+      {0.1, 0.1, 0.5, 0.1, 0.1},
+      {0.2, 0.5, 1, 0.5, 0.2},
+      {0.5, 1, 1, 1, 0.5},
+      {0.5, 1, 1, 1, 0.5}};
 
   for (int i = 0; i < WIDTH; i++)
   {
     for (int j = 0; j < 4; j++)
     {
-      if(mask[j][i] > 0.0)
+      if (mask[j][i] > 0.0)
       {
         int _color[3];
         int _ct = rand() % 2;
         int _rc = 4 + rand() % 4;
 
-        switch (_ct)
+        for (int p = 0; p < 3; p++)
         {
-
-          case 0:
-            _color[0] = (int)(((float)200 / (float)_rc)*mask[j][i]);
-            _color[1] = (int)(((float)44 / (float)_rc)*mask[j][i]);
-            _color[2] = (int)(((float)12 / (float)_rc)*mask[j][i]);
-            break;
-
-          case 1:
-            _color[0] = (int)(((float)100 / (float)_rc)*mask[j][i]);
-            _color[1] = (int)(((float)55 / (float)_rc)*mask[j][i]);
-            _color[2] = 0;
-            break;
-
-          case 2:
-            _color[0] = (int)(((float)245 / (float)_rc)*mask[j][i]);
-            _color[1] = (int)(((float)245 / (float)_rc)*mask[j][i]);
-            _color[2] = (int)(((float)12 / (float)_rc)*mask[j][i]);
-            break;
+          _color[p] = (int)(((float)palette[_ct][p] / (float)_rc) * mask[j][i]);
         }
+
         _postProcess[j][i][0] = _color[0];
         _postProcess[j][i][1] = _color[1];
         _postProcess[j][i][2] = _color[2];
       }
     }
   }
+}
 
+float _brightness = (MAX_BRIGHTNESS - MIN_BRIGHTNESS) / 2;
+float _brigntnessSpeedChange = 10.00;
+
+void changeBrightness()
+{
+  // brightness change over time
+
+  // pingpong effect
+
+  if (_brightness > MAX_BRIGHTNESS)
+  {
+    _brightness = (float)MAX_BRIGHTNESS;
+    _brigntnessSpeedChange = -_brigntnessSpeedChange;
+  }
+
+  if (_brightness < MIN_BRIGHTNESS)
+  {
+    _brightness = (float)MIN_BRIGHTNESS;
+    _brigntnessSpeedChange = -_brigntnessSpeedChange;
+  }
+
+  // or reverse randomly
+
+  if (rand() % 100 < 10)
+  {
+    _brigntnessSpeedChange = -_brigntnessSpeedChange;
+  }
+
+  // change brightness
+
+  _brightness += _brigntnessSpeedChange;
+
+  FastLED.setBrightness((int)_brightness);
 }
 
 void postProcess()
 {
   postProcessCnt++;
 
-  if(postProcessCnt>=POSTPROCESS_CYCLE)
+  if (postProcessCnt >= POSTPROCESS_CYCLE)
   {
-    postProcessCnt=0;
+    postProcessCnt = 0;
     postProcessCalculate();
+    changeBrightness();
   }
 
   for (int i = 0; i < WIDTH; i++)
   {
     for (int j = 0; j < 4; j++)
     {
-        setLedByCoord(i, HEIGHT-4+j, _postProcess[j][i]);
+      setLedByCoord(i, HEIGHT - 4 + j, _postProcess[j][i]);
     }
   }
-
 }
 
 /**
@@ -270,11 +276,11 @@ int getSafePixelData(int x, int y, int colorIndex)
   y = y < 0 ? 0 : y;
   y = y > HEIGHT - 1 ? HEIGHT - 1 : y;
   /*
-  if( x<0 || x>WIDTH-1 || y<0 || y>HEIGHT-1)
-  {
-    return 0;
-  }
-  */
+	if( x<0 || x>WIDTH-1 || y<0 || y>HEIGHT-1)
+	{
+		return 0;
+	}
+	*/
   return display[x][y][colorIndex];
 }
 
@@ -328,15 +334,8 @@ void interpolate()
   copyBufferToDisplay();
 }
 
-int _brightness = 0;
-
 void loop()
 {
-  /*
-  _brightness ++;
-  _brightness = _brightness>255 ? 0 : _brightness;
-  FastLED.setBrightness(_brightness);
-  */
   addNewParticle();
   interpolate();
   show();
